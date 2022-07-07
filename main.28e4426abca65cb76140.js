c3cce6b5d749853396f3fe02b12f62f86c046d6b
omero2582 <iiomero2582ii@gmail.com>
2022-07-07 19:23:13 -0400

    Bootleg home module export with factory function
    Need to make it so this home module, and the rest to come, dont
    auto load and display on the document. Instead this home module,
    and the rest, should return an element that the caller (index.js)
    can then append it themselves to wherever they want to. Also not sure
    if I should probably instead explicitily need to load each module with
    .renderAll(). Since they wont be appended anyways. But yea its prob
    better to explicitily load them.

--- main.28e4426abca65cb76140.js
+++ main.28e4426abca65cb76140.js
@@ -529,6 +529,99 @@ function styleTagTransform(css, styleElement) {
 
 module.exports = styleTagTransform;
 
+/***/ }),
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const Home = () => {
+    const main = document.querySelector('.main');
+
+    const clear = ()  =>{
+        main.innerHTML = '';
+    };
+
+    const renderIntro = () => {
+        const intro = document.createElement('div');
+        intro.classList.add('intro');
+    
+        const h1 = document.createElement('h1');
+        h1.textContent = "Omero's Kingdom";
+    
+        const p1 = document.createElement('p');
+        p1.textContent = "Omero's Kingdom has been in business since 2022 and it serves the best food on \
+        the planet u can ask Obama and he will vouch for me. 10/5 stars on Yelp reviews \
+        no cap potentially on a stack.";
+    
+        intro.appendChild(h1);
+        intro.appendChild(p1);
+        main.appendChild(intro);
+    };
+
+    const renderHours = () => {
+        const hours = document.createElement('div');
+        hours.classList.add('hours');
+    
+        const h2 = document.createElement('h2');
+        h2.textContent = 'Hours of Operation';
+        const schedule = document.createElement('div');
+        schedule.classList.add('schedule');
+    
+        const sunThu = document.createElement('p');
+        sunThu.textContent = 'Sun-Thu:';
+        const times1 = document.createElement('p');
+        times1.textContent = '1pm - 11pm';
+        const friSat = document.createElement('p');
+        friSat.textContent = 'Fri-Sat:'
+        const times2 = document.createElement('p');
+        times2.textContent = '1pm - 1am';
+    
+        schedule.appendChild(sunThu);
+        schedule.appendChild(times1);
+        schedule.appendChild(friSat);
+        schedule.appendChild(times2);
+    
+        hours.appendChild(h2);
+        hours.appendChild(schedule);
+        main.appendChild(hours);
+    };
+   
+    const renderAll = () => {
+        clear();
+        renderIntro();
+        renderHours();
+        renderLocation();
+        console.log('renderHome');
+    };
+
+    const renderLocation = () =>{
+        const location = document.createElement('div');
+        location.classList.add('location');
+
+        const h2 = document.createElement('h2');
+        h2.textContent = 'Location';
+        const p1 = document.createElement('p');
+        p1.textContent = '1120 Nicetryguy Ave.';
+
+        location.appendChild(h2);
+        location.appendChild(p1);
+        main.appendChild(location);
+    };
+
+    renderAll();
+    
+    return {clear, renderAll, renderIntro, renderHours, renderLocation};
+}; 
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);
+
 /***/ })
 
 /******/ 	});
@@ -612,87 +705,15 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
+/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home */ "./src/home.js");
 
 console.log('hai');
 
-const Home = (() => {
-    const main = document.querySelector('.main');
 
-    const clear = ()  =>{
-        main.innerHTML = '';
-    };
+let home = (0,_home__WEBPACK_IMPORTED_MODULE_1__["default"])();
 
-    const renderIntro = () => {
-        const intro = document.createElement('div');
-        intro.classList.add('intro');
-    
-        const h1 = document.createElement('h1');
-        h1.textContent = "Omero's Kingdom";
-    
-        const p1 = document.createElement('p');
-        p1.textContent = "Omero's Kingdom has been in business since 2022 and it serves the best food on \
-        the planet u can ask Obama and he will vouch for me. 10/5 stars on Yelp reviews \
-        no cap potentially on a stack.";
-    
-        intro.appendChild(h1);
-        intro.appendChild(p1);
-        main.appendChild(intro);
-    };
-
-    const renderHours = () => {
-        const hours = document.createElement('div');
-        hours.classList.add('hours');
-    
-        const h2 = document.createElement('h2');
-        h2.textContent = 'Hours of Operation';
-        const schedule = document.createElement('div');
-        schedule.classList.add('schedule');
-    
-        const sunThu = document.createElement('p');
-        sunThu.textContent = 'Sun-Thu:';
-        const times1 = document.createElement('p');
-        times1.textContent = '1pm - 11pm';
-        const friSat = document.createElement('p');
-        friSat.textContent = 'Fri-Sat:'
-        const times2 = document.createElement('p');
-        times2.textContent = '1pm - 1am';
-    
-        schedule.appendChild(sunThu);
-        schedule.appendChild(times1);
-        schedule.appendChild(friSat);
-        schedule.appendChild(times2);
-    
-        hours.appendChild(h2);
-        hours.appendChild(schedule);
-        main.appendChild(hours);
-    };
-   
-    const renderAll = () => {
-        clear();
-        renderIntro();
-        renderHours();
-        renderLocation();
-    };
-
-    const renderLocation = () =>{
-        const location = document.createElement('div');
-        location.classList.add('location');
-
-        const h2 = document.createElement('h2');
-        h2.textContent = 'Location';
-        const p1 = document.createElement('p');
-        p1.textContent = '1120 Nicetryguy Ave.';
-
-        location.appendChild(h2);
-        location.appendChild(p1);
-        main.appendChild(location);
-    };
-
-    renderAll();
-    
-})(); 
 })();
 
 /******/ })()
 ;
-//# sourceMappingURL=main.d3ccb4ceb73434a74b3b.js.map
+//# sourceMappingURL=main.28e4426abca65cb76140.js.map
